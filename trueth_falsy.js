b100d9b5d70e4c9b2d7320dfcc9d281fbee04db7
CoderMohammadShuvo <mohammad.shuvo4099@outlook.com>
2021-08-04 00:42:53 +0600

    add new file

---
+++ trueth_falsy.js
@@ -0,0 +1,20 @@
+//falsy
+// 0
+//''
+//false
+//undefined
+//NaN
+//null
+
+
+
+
+const name = null;
+
+
+if(name){
+    console.log("condition is true");
+}
+else{
+ console.log('condition is false');
+}
